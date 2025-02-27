4544005c838cf0ccd90885fef462398aa8e1461a
Arjun C Unni <arjuncrevathi@gmail.com>
2025-02-27 12:24:57 +0400

    Made pnpm work

---
+++ app/ui/SustainsLogo.tsx
@@ -0,0 +1,10 @@
+import { GlobeAltIcon } from '@heroicons/react/24/outline';
+
+export default function SustainsLogo() {
+    return (
+        <div className="flex flex-row items-center leading-none text-[#0ABF53] font-sans">
+            <GlobeAltIcon className="h-12 w-12 rotate-[15deg]" />
+            <p className="text-[44px] font-bold">Sustains.ai</p>
+        </div>
+    );
+}
